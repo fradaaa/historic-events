b8b477b4f05100a691fbe470afe3048d3b537989
Ruslan Bilalov <47795314+fradaaa@users.noreply.github.com>
2023-10-25 22:51:37 +0800

    Add CSS Reset & Update Global Styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
+import "modern-normalize";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./globals.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
