a40b61da6597c271e79c57098dc8fa620c043023
Ruslan Bilalov <47795314+fradaaa@users.noreply.github.com>
2023-10-26 20:40:35 +0800

    Add Mobile Events Slider

---
+++ src/components/MobileEvents/MobileEvents.tsx
@@ -0,0 +1,24 @@
+import EventsSlider from "../EventsSlider/EventsSlider";
+import Fade from "../Fade/Fade";
+import styles from "./MobileEvents..module.css";
+
+type MobileEventsProps = {
+  title: string;
+  show: boolean;
+  events: TimeSliceEvent[];
+};
+
+const MobileEvents = ({ title, show, events }: MobileEventsProps) => {
+  return (
+    <div className={styles.wrapper}>
+      <Fade show={show}>
+        <div className={styles.titleWrapper}>
+          <p className={styles.title}>{title}</p>
+        </div>
+        <EventsSlider events={events} slidesPerView={1.5} />
+      </Fade>
+    </div>
+  );
+};
+
+export default MobileEvents;
